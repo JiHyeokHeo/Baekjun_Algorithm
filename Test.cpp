0d6a83d39a815e72f475df23ab21abbd25edeecf
JiHyeok Heo <vndj1212@gmail.com>
2023-10-10 12:28:06 +0900

    2884번 문제

--- Test.cpp
+++ Test.cpp
@@ -3,9 +3,34 @@ using namespace std;
 
 int main(void)
 {
-    int H = 24;
-    int M = 60;
+    // 45분 일찍 일어나게 세팅
+    int H;
+    int M;
 
-        
+    cin >> H;
+    cin >> M;
+
+    if(M < 45)
+    {
+        H -=1;
+    }
+
+    if(H < 0)
+    {
+        H += 24;
+    }
+
+    M -= 45;
+
+    if(M < 0)
+    {
+        M += 60;
+    }
+    
+
+    cout<< H << " " << M <<endl;
+
+
+    
     return 0;
 }
