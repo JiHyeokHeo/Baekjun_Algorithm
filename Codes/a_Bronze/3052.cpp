87d6e9ec4953e4edc3d5b6e70b1a64d51bb31128
JiHyeok Heo <vndj1212@gmail.com>
2023-10-10 14:02:28 +0900

    3052번

--- Codes/a_Bronze/3052.cpp
+++ Codes/a_Bronze/3052.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(void)
+int main()
 {
-    
+    int nums[10];
+    int remains[10];
+
+    int remainCnt[100] = {};
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        int remain =  nums[i] % 42;
+        remains[i] = remain;
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        remainCnt[remains[i]]++;
+    }
+
+    int cnt = 0;
+    for(int i=0; i<100; i++)
+    {
+        if(remainCnt[i] >= 1)
+        {
+            cnt++;
+        }
+    }
+
+
+    cout<< cnt;
     return 0;
 }
